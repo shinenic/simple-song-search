48af6908e14d567e9fc2b7f83313e3a35d95b34b
tim <galadiya41@gmail.com>
2019-11-04 17:38:28 +0800

    Updates

---
+++ precache-manifest.81f02d7314c72288e9cb416e155ee901.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45527e30affb2e0b2594024ca4882564",
+    "url": "/simple-song-search/index.html"
+  },
+  {
+    "revision": "0abe05a2da807664a35d",
+    "url": "/simple-song-search/static/css/main.23883086.chunk.css"
+  },
+  {
+    "revision": "41439c5ba4dbe28a4fef",
+    "url": "/simple-song-search/static/js/2.4be2b892.chunk.js"
+  },
+  {
+    "revision": "0abe05a2da807664a35d",
+    "url": "/simple-song-search/static/js/main.33524ee0.chunk.js"
+  },
+  {
+    "revision": "135ccc00f4ba7403f629",
+    "url": "/simple-song-search/static/js/runtime-main.00845c7b.js"
+  },
+  {
+    "revision": "7914eafcfd03662cc597dde576a7c24c",
+    "url": "/simple-song-search/static/media/cross.7914eafc.svg"
+  },
+  {
+    "revision": "ade695e1c9d8433cda8b93992041f6d6",
+    "url": "/simple-song-search/static/media/menu.ade695e1.svg"
+  },
+  {
+    "revision": "3267ec2981a332adfabdfa685734b285",
+    "url": "/simple-song-search/static/media/search.3267ec29.svg"
+  }
+]);
